89e8a9e46d1b35af4dba3f52d44fc05ddf9c4f66
matvey <matgr2n@gmail.com>
2023-12-26 18:13:09 +0300

    changed model

--- src/integrations/models.py
+++ src/integrations/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class CallInfo(models.Model):
     organisation_name = models.CharField(max_length=255)
     organisation_phone = models.CharField(max_length=20)
-    comment = models.CharField()
+    comment = models.CharField(blank=True, null=True)
     call_id = models.CharField()
     scenario_id = models.CharField()
