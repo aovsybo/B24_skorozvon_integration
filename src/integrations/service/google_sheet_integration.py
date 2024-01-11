6d61a5835719e3841709b4a37d887c6172131862
matvey <matgr2n@gmail.com>
2024-01-11 13:51:17 +0300

    changed back

--- src/integrations/service/google_sheet_integration.py
+++ src/integrations/service/google_sheet_integration.py
@@ -50,14 +50,19 @@ def get_funnel_info_from_integration_table():
     table = get_table_data(settings.INTEGRATIONS_SPREADSHEET_ID, settings.INTEGRATIONS_SHEET_NAME)
     df = pd.DataFrame(table[2:], columns=table[1])
     request_columns = [
-        'Проекты',
+        'Все проекты на 13.12',
+        'Статус',
         'ID Стадии',
         'Ссылка на таблицу лидов [предыдущие]',
         'Название листа',
         'Названия прошлых листов',
         'Телеграм бот:'
     ]
-    return df[request_columns]
+    return df[df['Статус'].isin([
+        'Подключить',
+        'Подключить Приоритет 2',
+        'Подключить Приоритет 3'
+    ])][request_columns]
 
 
 def validate_data(fields: dict, stage_id: str):
